68436fe2bd115f142735e7929436e268544b6a95
Karim Serbouty <kserbouty.dev@protonmail.com>
2023-02-27 10:10:47 +0100

    Fix unicode on json response

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -13,13 +13,13 @@ class Response
         header_remove();
         header('Access-Control-Allow-Origin: *');
         header('Cache-Control: max-age=36000');
-        header('Content-Type: application/json');
+        header('Content-Type: application/json; charset=utf8');
 
         http_response_code($status);
 
         try {
 
-            $response = json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+            $response = json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
         } catch (\JsonException $exception) {
             echo "Error #" . $exception->getCode() . " : " . $exception->getMessage();
