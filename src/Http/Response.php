66c26966fe4a32abafdd4b44e1ba5b87f77bf6d9
Karim Serbouty <kserbouty.dev@protonmail.com>
2023-03-11 09:57:49 +0100

    Fix escaped slashes

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -17,7 +17,7 @@ class Response
 
         try {
 
-            $response = json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            $response = json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
         } catch (\JsonException $exception) {
             $response =  "Error #" . $exception->getCode() . " : " . $exception->getMessage() . ".";
